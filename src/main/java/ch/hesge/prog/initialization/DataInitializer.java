94632d9698cf3340cc0c5a76f751243afdc44154
Chadi Aebi <chadi-aebi@hotmail.com>
2020-01-10 17:19:26 +0100

    Landing Redirect, Selection de produits Accueil
      + index.jsp redirige vers Accueil
      + Page d'accueil recupere selection des produits
      + main.css

--- src/main/java/ch/hesge/prog/initialization/DataInitializer.java
+++ src/main/java/ch/hesge/prog/initialization/DataInitializer.java
@@ -7,6 +7,8 @@ import javax.ejb.EJB;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebListener
 public class DataInitializer implements ServletContextListener {
@@ -16,11 +18,11 @@ public class DataInitializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        productService.saveProduct(new Product("chat1", "Chat méchant", 100.0, "../ressources/product2.jpg"));
-        productService.saveProduct(new Product("chat2", "Chat méchant", 200.0, "../ressources/product2.jpg"));
-        productService.saveProduct(new Product("chat3", "Chat méchant", 300.0, "../ressources/product2.jpg"));
-        productService.saveProduct(new Product("chat4", "Chat méchant", 400.0, "../ressources/product2.jpg"));
-        productService.saveProduct(new Product("chat5", "Chat méchant", 500.0, "../ressources/product2.jpg"));
+        productService.saveProduct(new Product("Fit Cat", "Chat méchant", 100.0, "../ressources/product2.jpg"));
+        productService.saveProduct(new Product("DJ Cat", "Chat méchant", 200.0, "../ressources/product2.jpg"));
+        productService.saveProduct(new Product("Kit Cat", "Chat méchant", 300.0, "../ressources/product2.jpg"));
+        productService.saveProduct(new Product("CAT", "Chat méchant", 400.0, "../ressources/product2.jpg"));
+        productService.saveProduct(new Product("Cat Cat", "Chat méchant", 500.0, "../ressources/product2.jpg"));
     }
 
     @Override
--- src/main/java/ch/hesge/prog/servlet/AccueilServlet.java
+++ src/main/java/ch/hesge/prog/servlet/AccueilServlet.java
@@ -1,18 +1,35 @@
 package ch.hesge.prog.servlet;
 
+import ch.hesge.prog.model.Product;
+import ch.hesge.prog.service.ProductService;
+
 import javax.annotation.Resource;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 //jdbc:hsqldb:file:/Users/krys/Library/Caches/IntelliJIdea2019.2/tomcat/TomEE_8_5_41_projetJavaee_2/data/hsqldb/hsqldb
 public class AccueilServlet extends HttpServlet {
 
+    final int SELECTION_SIZE = 3;
+
+    @EJB
+    private ProductService productService;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Product> products = productService.getProducts();
+        List selection= new ArrayList();
+        for(int i=0;i<SELECTION_SIZE;i++){
+            selection.add(products.get(i));
+        }
+        req.setAttribute("selection", selection);
         req.getRequestDispatcher("/WEB-INF/Accueil.jsp").forward(req, resp);
     }
 }
